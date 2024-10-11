f23932dbfdc859e00ff9df20241fb25cdff3c86e
k0shinus <xrombi4@gmail.com>
2024-10-11 17:35:04 +0500

    Save commit

---
+++ scripts/global_map.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+@export var noise_height_text: NoiseTexture2D
---
+++ scripts/main_menu.gd
@@ -0,0 +1,35 @@
+extends Control
+
+#-------------------Play button logic-------------------
+func _on_play_pressed() -> void:
+	#var dialog = AcceptDialog.new()
+	#dialog.set_title("")
+	#dialog.set_text("You clicked play!")
+	#get_tree().current_scene.add_child(dialog)  # Add dialog to the current scene
+	#dialog.popup_centered()  # Show the dialog centered on the screen
+	get_tree().change_scene_to_file("res://scenes/global_map.tscn")
+
+#-------------------Quit button logic-------------------
+func form_quit_dialog() -> ConfirmationDialog:
+	var d = ConfirmationDialog.new()
+	d.set_title("")
+	d.set_text("Are you really want to quit?")
+	
+	# Connect the signals for OK and Cancel buttons
+	d.get_ok_button().pressed.connect(_on_confirmed)
+	d.get_cancel_button().pressed.connect(_on_canceled)
+	
+	return d
+
+func _on_quit_pressed() -> void:
+	var dialog = form_quit_dialog()
+	get_tree().current_scene.add_child(dialog)  # Add dialog to the current scene
+	dialog.popup_centered()  # Show the dialog centered on the screen
+
+# This function will be called when OK is pressed
+func _on_confirmed() -> void:
+	get_tree().quit()  # This will close the game
+
+# This function will be called when Cancel is pressed
+func _on_canceled() -> void:
+	pass  # Do nothing, just close the dialog
