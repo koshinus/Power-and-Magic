163f17a31f664414144dbbe324dcc36718846647
k0shinus <xrombi4@gmail.com>
2025-02-18 01:49:39 +0500

    Added test png. Debugged grayscaling and drawing circle on the image.

--- scripts/test_node.gd
+++ scripts/test_node.gd
@@ -1,84 +1,35 @@
 extends Node2D
 
-@onready var map_image : Image = Image.create_empty(
-						GLOBAL_MAP.LOCAL_MAP_WIDTH*GLOBAL_MAP.LOCAL_TILE_SIZE_IN_PIXELS,
-						GLOBAL_MAP.LOCAL_MAP_HEIGHT*GLOBAL_MAP.LOCAL_TILE_SIZE_IN_PIXELS,
-						false,
-						$TestLayer.tile_set.get_source( 0 ).texture.get_image().get_format()
-						)
+var img : ImageTexture = ImageTexture.create_from_image(
+			Image.load_from_file( "res://resources/samples/flower_field.png" ) )
 
-## Generating tile type for local adventure map:
-## >=50% - should be of type that big_tile_type have
-## other part depends on the big tile neighbours
-## for example, if tile is grass, other 4 tiles are also grass
-## but also there are 2 steppens and 2 deserts then:
-## 50 + 6.25*4 - grass local tile chance
-## 6.25*2 - steppen and desert chances for each
-func _get_local_tile_type_by_neighbours( big_tile_type : int, neighbours : Array[ int ] ) -> int:
-	neighbours.sort()
-	var tiles_arr : Array[int]
-	tiles_arr.resize( neighbours.size() )
-	tiles_arr.fill( big_tile_type )
-	var idx = neighbours.find( big_tile_type )
-	if idx == -1:
-		neighbours.append_array( tiles_arr )
-	else:
-		for i in tiles_arr:
-			neighbours.insert( idx, i )
-	return neighbours[ randi() % neighbours.size() ]
+func _circled_texture( txtr : ImageTexture, color : Color, radius : int, center : Vector2 ) -> ImageTexture:
+	var res_img : Image = txtr.get_image()
+	var size : Vector2i = txtr.get_size()
+	print("Size is", size)
+	for y in range(size.y):
+		for x in range(size.x):
+			var dist : float = ( Vector2(x, y) - center ).length()
+			# If print only equal to the radius then circle will not be seen
+			if dist <= radius and dist >= radius-3:
+				res_img.set_pixel( x, y, color )
+	return ImageTexture.create_from_image( res_img )
 	
-func _get_tile_offset_by_coords( x : int, y : int ) -> int:
-	var w : int = GLOBAL_MAP.LOCAL_MAP_WIDTH
-	var h : int = GLOBAL_MAP.LOCAL_MAP_HEIGHT
-	var rule_dict = {
-		Vector2i(0, 0) : GLOBAL_MAP.LOCAL_TILES.TOP_LEFT,
-		Vector2i(w-1, 0) : GLOBAL_MAP.LOCAL_TILES.TOP_RIGHT,
-		Vector2i(0, h-1) : GLOBAL_MAP.LOCAL_TILES.BOTTOM_LEFT,
-		Vector2i(w-1, h-1) : GLOBAL_MAP.LOCAL_TILES.BOTTOM_RIGHT
-	}
-	var dict_val = rule_dict.get( Vector2i( x, y ) )
-	if dict_val != null: return int(dict_val)
-	if y == 0: return GLOBAL_MAP.LOCAL_TILES.TOP
-	elif y == h-1: return GLOBAL_MAP.LOCAL_TILES.BOTTOM
-	elif x == 0:
-		if y % 2 == 1: return GLOBAL_MAP.LOCAL_TILES.LEFT_ODD
-		else: return GLOBAL_MAP.LOCAL_TILES.LEFT_EVEN
-	elif x == w-1:
-		if y % 2 == 1: return GLOBAL_MAP.LOCAL_TILES.RIGHT_ODD
-		else: return GLOBAL_MAP.LOCAL_TILES.RIGHT_EVEN
-	elif y % 2 == 1: return GLOBAL_MAP.LOCAL_TILES.MIDDLE_ODD
-	else: return GLOBAL_MAP.LOCAL_TILES.MIDDLE_EVEN
+func _grayscaled_texture( txtr : ImageTexture ) -> ImageTexture:
+	var res_img : Image = txtr.get_image()
+	var size : Vector2i = txtr.get_size()
+	for y in range(size.y):
+		for x in range(size.x):
+			var color : Color = res_img.get_pixel( x, y )
+			var grayed = ( color.r + color.g + color.b )/3
+			res_img.set_pixel( x, y, Color( grayed, grayed, grayed ) )
+	return ImageTexture.create_from_image( res_img )
 
-func get_tilemap_layer_data() -> PackedByteArray:
-	return $TestLayer.tile_map_data
+func _on_normal_pressed() -> void:
+	$TextureRect.texture = img
 
-func get_map_image() -> Image:
-	return map_image
+func _on_grey_pressed() -> void:
+	$TextureRect.texture = _grayscaled_texture( img )
 
-func get_map_image_format() -> Image.Format:
-	return map_image.get_format()
-
-func init_by_params( global_tile_type : int, neighbours : Array[int] ) -> void:
-	var w : int = GLOBAL_MAP.LOCAL_MAP_WIDTH
-	var h : int = GLOBAL_MAP.LOCAL_MAP_HEIGHT
-	var tsize : int  = GLOBAL_MAP.TILE_SIZE_IN_TILESET
-	var src_image : Image = $TestLayer.tile_set.get_source( 0 ).texture.get_image()
-	map_image = Image.create_empty(
-						GLOBAL_MAP.LOCAL_MAP_WIDTH*GLOBAL_MAP.LOCAL_TILE_SIZE_IN_PIXELS,
-						GLOBAL_MAP.LOCAL_MAP_HEIGHT*GLOBAL_MAP.LOCAL_TILE_SIZE_IN_PIXELS,
-						false, src_image.get_format() )
-	print("Start initialization. Size is ", neighbours.size())
-	for y in range( h ):
-		for x in range( w ):
-			var tile_x_offset = _get_tile_offset_by_coords( x, y )
-			var tile_y_offset = _get_local_tile_type_by_neighbours( global_tile_type, neighbours.duplicate() )
-			var tileset_offset : Vector2i = Vector2i( tile_x_offset, tile_y_offset )
-			var tileset_offset_36 : Vector2i = Vector2i ( tile_x_offset*36, tile_y_offset*36 )
-			map_image.blit_rect( src_image, Rect2i( tileset_offset_36, Vector2i( tsize, tsize ) ),
-								Vector2i(x*w, y*h) )
-			$TestLayer.set_cell( Vector2i( x, y ), 0, tileset_offset )
-	map_image.resize( 64, 64, Image.INTERPOLATE_BILINEAR )
-	#AStarGrid2D
-	#var file_temp = "C:/Users/Vadim/Desktop/test/some_{0}_{1}.png"
-	#map_image.save_png( file_temp.format([0, 0]) )
-	#map_image.save_png( file_temp.format([0, 1]) )
+func _on_circuled_pressed() -> void:
+	$TextureRect.texture = _circled_texture( img, Color.GOLD, 100, Vector2( 300, 300 ) )
